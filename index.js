483e1c0d0150e8c1c44929fce546c7fc2fd53882
allaeldinelias <allaeldinelias@yahoo.com>
2015-01-20 17:30:57 -0500

    Added dependencies

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var express= require("express"),
     app=express(),
     server = require('http').createServer(app),
-    port = process.env.PORT ||8080;
+    port = process.env.PORT ||8080,
+    debug= require('debug')('index.js');
 
 server.listen(port, function (){
     console.log("Server Listing at port %d",port);
+    debug("Server Listing at port %d",port);
 });
 
+app.get("/student/:id",function(req, res){
+    res.end(req.params.id+ " elias!");
+});
 app.use(express.static(__dirname + '/www/'));
